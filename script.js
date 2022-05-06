74f4979a943343a994155843e84c4216dba58bae
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-05-06 22:11:04 +0300

    Handle Data from Form and form validation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const modal = document.getElementById("modal");
 const modalShow = document.getElementById("modal-show");
 const modalClose = document.getElementById("modal-close");
-const bookmarkForm = document.getElementById("bookmark-from");
+const bookmarkForm = document.getElementById("bookmark-form");
 const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
@@ -12,7 +12,7 @@ function showModal() {
 }
 
 function closeModal() {
-    modal.classList.remove("modal-show");
+    modal.classListNaNpxove("modal-show");
 }
 
 modalShow.addEventListener("click", showModal);
@@ -20,3 +20,37 @@ modalClose.addEventListener("click", closeModal);
 window.addEventListener("click", (e) => {
     e.target.id === "modal" ? closeModal() : false;
 });
+
+// Validate form
+function validate(nameValue, urlValue) {
+    const expression =
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert("Empty value");
+        return false;
+    }
+
+    if (!urlValue.match(regex)) {
+        alert("URL is not valid");
+    }
+
+    return true;
+}
+
+// Handle Data from Form
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlName = websiteUrlEl.value;
+
+    if (!urlName.includes("http://") && !urlName.includes("https://")) {
+        urlName = `https://${urlName}`;
+    }
+
+    if (!validate(nameValue, urlName)) {
+        return false;
+    }
+}
+
+bookmarkForm.addEventListener("submit", storeBookmark);
